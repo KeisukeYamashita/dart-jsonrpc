c809b69330d59b51e5301e1b35f9ca1221597542
KeisukeYamashita <19yamashita15@gmail.com>
2018-06-19 19:47:06 +0900

    :+1: add http request methods

--- lib/jsonrpc.dart
+++ lib/jsonrpc.dart
@@ -1,3 +1,5 @@
-void main(){
-  print("object");
-}
+// Copyright (c) 2018, the project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// Apache-2.0 license that can be found in the LICENSE file.
+
+export 'jsonrpc/jsonrpc.dart';
---
+++ lib/jsonrpc/jsonrpc.dart
@@ -0,0 +1,32 @@
+// Copyright (c) 2018, the project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// Apache-2.0 license that can be found in the LICENSE file.
+import 'package:http/http.dart' as http;
+
+class JSONRPCClient {
+  String url;
+  Map headers;
+
+  JSONRPCClient(url){
+    this.url = url;
+  }
+
+// setHeader sets header for request
+  setHeaders(headers){
+    this.headers = headers;
+  }
+
+  call(method,params,callback(response)){
+    var timeStamp = new DateTime.now().millisecondsSinceEpoch;
+    var body = {
+      'jsonrpc': "2.0",
+      'method': method,
+      'params': params,
+      'id': timeStamp,
+    };
+    http.post(url,body:body)
+      .then((response){
+        callback(response);
+      });
+  }
+}
